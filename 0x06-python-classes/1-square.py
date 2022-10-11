66e9b32572809aaca3a874ac98cc66641e32680c
Osariemen Uwuilehue <106764970+Jojobutercup@users.noreply.github.com>
2022-10-11 18:09:55 -0500

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for private instant attribute size"""
+
+
+class Square:
+    """Defines a square object
+    """
+    def __init__(self, size):
+        """Initializes the size
+        Args:
+            param1 (int): size of the square
+        """
+        self.__size = size
